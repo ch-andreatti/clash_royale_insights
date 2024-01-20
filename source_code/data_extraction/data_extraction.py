61c2c1f3361b947f879458ac8b8cf6f0e3fc0729
Caio Henrique <caio.hda@gmail.com>
2024-01-20 11:23:58 -0300

    Added source_code and data_extraction

---
+++ source_code/data_extraction/data_extraction.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+# Load access token from access_token.json
+with open("./access_token.json", "r") as file:
+    access_token = json.load(file)
+token = access_token["token"]
+
+# Load personal tag from players_tag.json
+with open("./players_tag.json", "r") as file:
+    players_tag = json.load(file)
+personal_tag = players_tag["personal_tag"]
+
+headers = {"Authorization": f"Bearer {token}"}
+response = requests.get(f"https://api.clashroyale.com/v1/players/{personal_tag}", headers=headers)
+
+print(response.text)
